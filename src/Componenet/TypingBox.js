8f5e670b064fc7c68bbee974f6d4e25080c23bf8
Somesh Jaiswal <someshjaiswal98@gmail.com>
2023-10-15 00:06:16 +0530

    new2

--- src/Componenet/TypingBox.js
+++ src/Componenet/TypingBox.js
@@ -10,14 +10,9 @@ const TextAreaComponent = () => {
   };
 
   return (
-    <div>
-      <label>Text Area:</label>
-      <textarea
-        value={textValue}
-        onChange={handleTextAreaChange}
-        placeholder="The quick brown fox jumps over the lazy dog. This sentence contains all the letters of the alphabet, making it a pangram. Pangrams are often used to test fonts, keyboards, and typewriters. Typing exercises like these can help improve your typing speed and accuracy. It's essential to develop good typing skills in today's digital age, as typing is a fundamental skill for various tasks, from writing emails to coding and data entry."
-      />
-      <p>You typed: {textValue}</p>
+    <div className='textBox'>
+      <textarea value={textValue} onChange={handleTextAreaChange} placeholder="The quick brown fox jumps over the lazy dog. This sentence contains all the letters of the alphabet, making it a pangram. Pangrams are often used to test fonts, keyboards, and typewriters. Typing exercises like these can help improve your typing speed and accuracy. It's essential to develop good typing skills in today's digital age, as typing is a fundamental skill for various tasks, from writing emails to coding and data entry."/>
+      <p>{textValue}</p>
     </div>
   );
 };
